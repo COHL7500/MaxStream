3180a91fc7f5b0a6e217cc1df8ff44928f0a7523
COHL7500 <cohl7500@gmail.com>
2021-12-08 13:26:42 +0100

    - Added images folder with movies and series thumbnails - Set up structure for main .fxml file - Added top bar with "film", "serier", "min liste"

--- src/main/java/project_code/MainController.java
+++ src/main/java/project_code/MainController.java
@@ -1,4 +1,10 @@
 package project_code;
 
 public class MainController {
+
+    //TODO:
+    // Grouping the pictures
+    // Buttons for "film, serier, min liste"
+    // functionality for filtering movies
+    // showing all the movies
 }
