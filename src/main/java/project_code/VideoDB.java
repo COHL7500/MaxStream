c3c74caa621e0682862837ae2a213722a949c2c0
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2021-12-14 11:13:17 +0100

    BufferedReader setup + images

--- src/main/java/project_code/VideoDB.java
+++ src/main/java/project_code/VideoDB.java
@@ -1,6 +1,6 @@
 package project_code;
 
-import javafx.scene.control.Button;
+//import javafx.scene.control.Button;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,28 +9,70 @@ import java.util.Map;
 
 public class VideoDB {
 
-    HashMap<Video, Button> videoDB = new HashMap<Video, Button>();
+   // HashMap<Video, Button> videoDB = new HashMap<Video, Button>();
 
     public void videoDB()
     {
     }
 
-
     final static String filePath = "temp.txt";
 
-    public static Map<String, String> MapFromFile()
-    {
-        Map<String, String> map = new HashMap<String, String>();
+    public static ArrayList<Video> listOfMovies() throws FileNotFoundException {
+        ArrayList<Video> movieList = new ArrayList<>();
 
         BufferedReader br = null;
+        try {
+            FileReader file = new FileReader(filePath);
 
+            br = new BufferedReader(file);
 
-        return map;
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+
+                String title = parts[0].trim();
+                String year = parts[1].trim();
+
+                int i = Integer.parseInt(year);
+
+                String genres = parts[2].trim();
+                String[] genre = genres.split(",");
+
+                String rating = parts[3].trim();
+
+                double d = Double.parseDouble(rating);
+
+                if (!title.equals("") && !year.equals("") && !genres.equals("") && !rating.equals(""))
+                {
+                    Movie m = new Movie(title, i, genre, "", d);
+
+                    movieList.add(m);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        } finally {
+            if (br != null) {
+                try
+                {
+                    br.close();
+                } catch (Exception e)
+                {
+                }
+            }
+        }
+        return movieList;
     }
 
-
-
-
-
-
 }
+
+
+
+
+
+
+
